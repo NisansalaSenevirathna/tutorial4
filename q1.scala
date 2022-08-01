653ab8405e64b42811ece441821761b290e87fbd
NisansalaSenevirathna <92750030+NisansalaSenevirathna@users.noreply.github.com>
2022-08-01 22:52:50 +0530

    Update q1.scala

--- q1.scala
+++ q1.scala
@@ -1,10 +1,10 @@
 object q1 extends App{
 
   def interest(amount:Double):Double = amount match{
-         
-         case x if x<=20000 =>x*.02;
-         case x if x>=200000=>x*.04;
-         case x if x>=2000000=>x*.035;
+         case x if x<0=>0
+         case x if x>20000 =>x*.02;
+         case x if x>200000=>x*.04;
+         case x if x>2000000=>x*.035;
          case x if x>20000000=>x*.065;
   }
      
